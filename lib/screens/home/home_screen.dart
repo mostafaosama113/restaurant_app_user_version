79da6fca7e86c3f3283863812fcf4173cc2e34b3
mostafa <hitmanx97@gmail.com>
2022-03-08 14:52:58 +0200

    home provider

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app/widgets/progress_widget.dart';
+
+import 'home_provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      drawer: Drawer(
-        child: Column(),
+    return ChangeNotifierProvider(
+      create: (context) => HomeProvider(),
+      builder: (context, child) => Consumer<HomeProvider>(
+        builder: (context, model, child) => Stack(
+          children: [
+            Scaffold(
+              appBar: AppBar(),
+              drawer: Drawer(
+                child: Column(),
+              ),
+            ),
+            if (model.isLoading) progressWidget(context),
+          ],
+        ),
       ),
     );
   }
